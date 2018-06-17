a37837e9ec1881e475c44c58495f28af8cb2afed
dustinpfister <dustin.pfister@gmail.com>
2018-06-17 18:04:14 -0400

    statred jumper string file

--- js/whitebox/wb_jumper_string.js
+++ js/whitebox/wb_jumper_string.js
@@ -3,10 +3,19 @@
     if (w.wb === undefined)
         w.wb = {};
 
+    // pad string
+    var pad = function (n, width, z) {
+        z = z || '0';
+        n = n + '';
+        return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+    };
+
     // Jumper String Class
-    wb.JumperString = function () {
+    wb.JumperString = function (string) {
 
         this.string = string || '0000000000000000';
+
+        this.string = pad(string, 16, '0');
 
     };
 
