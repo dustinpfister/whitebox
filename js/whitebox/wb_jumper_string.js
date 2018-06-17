b811d7a4995b84790bb7bea87f67e0e89eb96de7
dustinpfister <dustin.pfister@gmail.com>
2018-06-17 18:18:24 -0400

    have a starting idea of how to break down the jumper string

--- js/whitebox/wb_jumper_string.js
+++ js/whitebox/wb_jumper_string.js
@@ -13,9 +13,14 @@
     // Jumper String Class
     wb.JumperString = function (string) {
 
+        // use given string, or default to all zeros
         this.string = string || '0000000000000000';
 
+        // pad the string
         this.string = pad(string, 16, '0');
+
+        // game mode is last three bits
+        this.gameMode = parseInt(this.string.slice(13, 16), 2);
 
     };
 
